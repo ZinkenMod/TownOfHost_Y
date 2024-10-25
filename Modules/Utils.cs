tukasa0001/TownOfHost

ログフォルダを移動

Modules/Utils.cs
@@ -1046,7 +1046,7 @@ namespace TownOfHost
         }
         public static DirectoryInfo GetLogFolder(bool auto = false)
         {
-            var folder = Directory.CreateDirectory("TOH_LOGS");
+            var folder = Directory.CreateDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/TownOfHost/Logs");
             if (auto)
             {
                 folder = Directory.CreateDirectory($"{folder.FullName}/AutoLogs");

ZinkenMod/TownOfHost_Y

Modules/Utils.cs
@@ -1551,7 +1551,7 @@ public static class Utils
     }
     public static DirectoryInfo GetLogFolder(bool auto = false)
     {
-        var folder = Directory.CreateDirectory("TOH_LOGS");
+        var folder = Directory.CreateDirectory($"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/TownOfHost_Y/Logs");
         if (auto)
         {
             folder = Directory.CreateDirectory($"{folder.FullName}/AutoLogs");